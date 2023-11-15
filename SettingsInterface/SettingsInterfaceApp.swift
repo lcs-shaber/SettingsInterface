fc025a69e03f3d00738c74215bfcacfd5386adf1
Sophie Haber <SHaber@lcs.on.ca>
2023-11-15 12:36:34 -0500

    Initial Commit

---
+++ SettingsInterface/SettingsInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsInterfaceApp.swift
+//  SettingsInterface
+//
+//  Created by Sophie Haber on 15.11.23.
+//
+
+import SwiftUI
+
+@main
+struct SettingsInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
